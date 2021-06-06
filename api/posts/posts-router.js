5883c780ad2b9544b1a2254e98671845704c2a92
Polly Yospan <porgroovy@gmail.com>
2021-06-06 15:04:17 -0400

    create route for getting all posts, getting post by id, creating new post and updating post

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -57,4 +57,38 @@ router.post("/api/posts", async (req, res) => {
   }
 });
 
+// Update post
+router.put("/api/posts/:id", async (req, res) => {
+  const { id } = req.params;
+  const post = req.body;
+
+  try {
+    if (!post.title || !post.contents) {
+      res
+        .status(400)
+        .json({ message: "Please provide title and contents for the post" });
+    } else {
+      const updatedPost = await db.update(id, post);
+      const newPost = {
+        ...updatedPost,
+        title: post.title,
+        contents: post.contents,
+      };
+      console.log(newPost);
+      if (!updatedPost) {
+        res
+          .status(404)
+          .json({ message: `The post with id ${id} does not exist` });
+      } else {
+        res.status(200).json(newPost);
+      }
+    }
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .json({ message: "The post information could not be modified" });
+  }
+});
+
 module.exports = router;
