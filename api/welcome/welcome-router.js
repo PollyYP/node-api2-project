3447a857a8855bfa74e1be7053b70746f50f2611
Polly Yospan <porgroovy@gmail.com>
2021-06-14 00:13:40 -0400

    create .env

--- api/welcome/welcome-router.js
+++ api/welcome/welcome-router.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {
-  try {
-    res.status(200).json({ message: "Welcome to node-api2-project API" });
-  } catch (err) {
-    next(err);
-  }
+router.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Welcome to node-api2-project API",
+    secret: `You now see a ${process.env.SECRET}` || "There is no secret",
+  });
 });
 
 module.exports = router;
